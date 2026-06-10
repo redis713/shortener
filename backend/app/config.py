b3fe21da49f1c1821274e8521cf31d3f102443e7
rea <rediskabatman@gmail.com>
2026-06-10 15:23:57 +0800

    added general .env file

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,11 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
 
-    # на будущее
     LINK_TTL_DAYS: int = 2
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
